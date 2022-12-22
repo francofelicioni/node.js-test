4a1bb189e7acb3c89f9b1464c864b65d2df2fb07
Franco Felicioni <fran.felicioni@gmail.com>
2022-12-22 10:20:32 +0100

    Csv output implemented

--- index.js
+++ index.js
@@ -3,46 +3,57 @@ const fs = require("fs");
 const data = fs
   .readFileSync("./countries.txt", { encoding: "utf8", flag: "r" })
   .replace(/,/g, "");
-const newData = data.slice(24).split(/\r?\n/);
+const newData = data.split(/\r?\n/);
+let title = newData.shift();
 
 const singleCountriesInfo = newData.map((data) => data.split(" "));
 
 const finalArray = [["Country", "Population", "Area", "Population Density"]];
 
-
 for (let index = 0; index < singleCountriesInfo.length - 1; index++) {
   const element = singleCountriesInfo[index];
 
-  let name = '';
+  let name = "";
 
   for (let i = 0; i < element.length; i++) {
     if (isNaN(element[i])) {
-      name += ' ' + (element[i]) + ' ';
+      name += " " + element[i] + " ";
     }
   }
 
-  let population = parseInt(element[element.length - 2]);
-  let area = parseInt(element[element.length - 1]);
-  let pd;
+  let population;
 
-  if (!isNaN(population) && population > 1 && !isNaN(area) && area > 1) {
-    pd = (population / area).toFixed(2);
+  if (element[element.length - 2] === "1") {
+    population = parseInt(element[element.length - 3]);
   } else {
-    pd = 0;
+    population = parseInt(element[element.length - 2]);
   }
 
-  finalArray.push([name.trim(), population, area, parseFloat(pd)])
+  let area;
+
+  if (element[element.length - 1] === "1") {
+    area = parseInt(element[element.length - 2]);
+  } else {
+    area = parseInt(element[element.length - 1]);
+  }
+
+  let pd;
+  let pdmi = "";
+
+  if (!isNaN(population) && !isNaN(area)) {
+    pd = (population / area).toFixed(2);
+  } else {
+    pdmi = "Wrong or missing information";
+  }
+
+  finalArray.push([name.trim(), population, area, pd ? parseFloat(pd) : pdmi]);
 }
 
 for (let index = 1; index < finalArray.length; index++) {
   const element = finalArray[index];
-  finalArray.sort((a,b) => b[element.length -1 ] -  a[element.length -1])
+  finalArray.sort((a, b) => b[element.length - 1] - a[element.length - 1]);
 }
 
-finalArray[1].sort((a,b) => b[finalArray.length -1 ] -  a[finalArray.length -1])
-
-console.dir(finalArray, { maxArrayLength: null });
-
-const cadena = finalArray.toString();
+const cadena = finalArray.map((country) => (country).join(' - ')  + '\n').join('\n');
 
 fs.writeFileSync("countries.csv", cadena);
